3c076aa2049e3bedc76321f07eda3f0b429b820c
RGM36 <92078564+RGM36@users.noreply.github.com>
2023-02-01 16:31:09 +0100

    Final + script reload + ecogestes
    Plz accept my pull request :(

--- server/.auto_daemon.sh
+++ server/.auto_daemon.sh
@@ -1,5 +1,12 @@
+
 #!/bin/env bash
 
 while true
-do curl  "http://isis.unice.fr/~mgautero/ext/sae302/bd/ecowatt.db" --output ecowatt.db 2>/dev/null && ./trans.sh && ./request.sh && date && ./.dump/00_vars.sh ; sleep 36000
+do curl  "http://isis.unice.fr/~mgautero/ext/sae302/bd/ecowatt.db" --output ecowatt.db 2>/dev/null &\
+& ./trans.sh &\
+& ./request.sh &\
+& date &\
+& ./.dump/00_vars.sh &\
+& ./ecogestes.sh
+sleep 36000
 done
