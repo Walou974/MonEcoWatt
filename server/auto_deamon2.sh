8ae4c67a8025612d684e107012d09b9938819053
Mael Albany <99243661+Walou974@users.noreply.github.com>
2023-03-15 19:40:41 +0100

    Delete server directory

--- server/auto_deamon2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl  "http://isis.unice.fr/~mgautero/ext/sae302/bd/ecowatt.db" --output ecowatt.db 2>/dev/null &&
-./trans.sh &&
-./request.sh &&
-date &&
-./.dump/00_vars.sh &&
-./ecogestes.sh
