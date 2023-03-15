13019353d3653cc222bf76b3e04b308ec282f130
RGM36 <92078564+RGM36@users.noreply.github.com>
2023-03-15 19:35:03 +0100

    PATCH
    Correction de l'erreur

---
+++ server/auto_deamon2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl  "http://isis.unice.fr/~mgautero/ext/sae302/bd/ecowatt.db" --output ecowatt.db 2>/dev/null &&
+./trans.sh &&
+./request.sh &&
+date &&
+./.dump/00_vars.sh &&
+./ecogestes.sh
